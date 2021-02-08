8a707b76eb585ea366c8e94dd2f8d4b360e7cb6e
Dominika Erdelyiova <dominika.erdelyiova@gmail.com>
2021-02-08 11:52:42 +0100

    Initial commit || package insertNewMonument

---
+++ src/insertNewMonument/Main.java
@@ -0,0 +1,9 @@
+package insertNewMonument;
+
+public class Main {
+    City newCity = new City("Sliac",289, "Bystricke okolie","Slovakia");
+        database.insertCity(newCity);
+
+        database.insertNewMonument("FRA" , "Paris","Eiffel Tower");
+}
+
